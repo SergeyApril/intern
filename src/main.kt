a40f9cf07b4750347f223c753b37a561b6494599
SergeyApril <yzsanis@yandex.ru>
2021-04-29 15:52:03 +0300

    This is a new commit in my first program

--- src/main.kt
+++ src/main.kt
@@ -2,4 +2,5 @@ fun main(args: Array<String>){
         println("Hello Kotlin")
         println("Hello Kostya")
         println("This is changes was made in newbranch!")
+         println("This is new changes was made in newbranch!")
     }
